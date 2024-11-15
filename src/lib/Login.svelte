45d18926a35a5b0e3402d2126ed7e3e3937aedbd
quinilo <hiquenvol@gmail.com>
2024-11-15 15:03:22 +0100

    Initial commit

---
+++ src/lib/Login.svelte
@@ -0,0 +1,12 @@
+<script>
+    import PocketBase from 'pocketbase';
+
+    async function auth() {
+        const pb = new PocketBase('http://quinilo.de:32772');
+
+        const authData = await pb.collection('note_users').authWithPassword(
+            'YOUR_USERNAME_OR_EMAIL',
+            'YOUR_PASSWORD',
+        );
+    }
+</script>
