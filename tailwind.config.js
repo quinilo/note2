10d23c8867a6513298e0c08cfe8aedbaf0f65f3d
quinilo <hiquenvol@gmail.com>
2024-11-16 22:41:19 +0100

    darkmode in app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
   plugins: [require('daisyui'),],
+  daisyui: {
+    themes: ["dark"],
+  },
 }
 
